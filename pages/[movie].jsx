285706a4c15da54574acd1b3ff67b4a924ab2b72
dafom <ayomidechollom@gmail.com>
2021-08-01 04:37:42 +0100

    feat: statically pre-rendering [movies] page

--- pages/[movie].jsx
+++ pages/[movie].jsx
@@ -3,23 +3,22 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import styles from '../styles/MovieDetails.module.css';
-const MovieList = ({}) => {
+const MovieList = ({ movieDetails }) => {
   //  get query
   const { movie } = useRouter().query;
   // movie state
-  const [movieState, setMovieState] = React.useState({});
   // fecth movie by id
-  const fecthMovieById = async () => {
-    const url = `
-    https://api.themoviedb.org/3/movie/${movie}?api_key=86e04c8b56fc4bc4b40c3e45f058a61d&language=en-US`;
-    try {
-      const { data } = await axios.get(url);
-      // set movie state
-      setMovieState(data);
-    } catch (e) {
-      console.log(e.message, ':::: error message ::::');
-    }
-  };
+  // const fecthMovieById = async () => {
+  //   const url = `
+  //   https://api.themoviedb.org/3/movie/${movie}?api_key=86e04c8b56fc4bc4b40c3e45f058a61d&language=en-US`;
+  //   try {
+  //     const { data } = await axios.get(url);
+  //     // set movie state
+  //     setMovieState(data);
+  //   } catch (e) {
+  //     console.log(e.message, ':::: error message ::::');
+  //   }
+  // };
   // currency formatter
   const currencyFormatter = (value) => {
     return value?.toLocaleString('en-US', {
@@ -32,52 +31,52 @@ const MovieList = ({}) => {
     return `${Math.round((value / 10) * 100)}%`;
   };
   // fetch data by id on mounted
-  React.useEffect(() => {
-    fecthMovieById();
-  }, []);
+  // React.useEffect(() => {
+  //   fecthMovieById();
+  // }, []);
   return (
     <div
       className={styles.detailsWrapper}
       style={{
-        backgroundImage: `url(http://image.tmdb.org/t/p/w185${movieState?.backdrop_path})`,
+        backgroundImage: `url(http://image.tmdb.org/t/p/w185${movieDetails?.backdrop_path})`,
       }}
     >
       <div className={styles.layer}></div>
       <div className={styles.bodyWrapper}>
         <div className={styles.posterImageWrapper}>
           <img
-            src={`http://image.tmdb.org/t/p/w185${movieState?.poster_path}`}
+            src={`http://image.tmdb.org/t/p/w185${movieDetails?.poster_path}`}
             alt=""
             className={styles.posterImage}
           />
         </div>
         <div className={styles.description}>
-          <h1>{movieState.title}</h1>
-          <i>{`"${movieState.tagline}"`}</i>
+          <h1>{movieDetails.title}</h1>
+          <i>{`"${movieDetails.tagline || 'none'}"`}</i>
           <h3>Overview</h3>
-          <p>{movieState.overview}</p>
+          <p>{movieDetails.overview}</p>
           <div className={styles.details}>
             <div className={styles.col}>
               <h3>Realease Date</h3>
-              <p>{movieState.release_date}</p>
+              <p>{movieDetails.release_date}</p>
               <h3>Popularity</h3>
-              <p>{movieState.popularity}</p>
+              <p>{movieDetails.popularity}</p>
               <h3>Genre</h3>
-              <p>{movieState?.genres?.map((item) => `${item.name},`)}</p>
+              <p>{movieDetails?.genres?.map((item) => `${item.name},`)}</p>
             </div>
             <div className={styles.col}>
               <h3>Average Vote</h3>
-              <p>{getPercentage(movieState.vote_average)}</p>
+              <p>{getPercentage(movieDetails.vote_average)}</p>
               <h3>Total Votes</h3>
-              <p>{movieState.vote_count}</p>
+              <p>{movieDetails.vote_count}</p>
               <h3>Original Language</h3>
-              <p>{movieState.original_language}</p>
+              <p>{movieDetails.original_language}</p>
             </div>
             <div className={styles.col}>
               <h3>Budget</h3>
-              <p>{currencyFormatter(movieState.budget)}</p>
+              <p>{currencyFormatter(movieDetails.budget)}</p>
               <h3>Revenue</h3>
-              <p>{currencyFormatter(movieState.revenue)}</p>
+              <p>{currencyFormatter(movieDetails.revenue)}</p>
             </div>
           </div>
         </div>
@@ -85,5 +84,36 @@ const MovieList = ({}) => {
     </div>
   );
 };
+export async function getStaticPaths() {
+  return {
+    paths: [], //indicates that no page needs be created at build time
+    fallback: 'blocking', //indicates the type of fallback
+  };
+}
+
+export async function getStaticProps(context) {
+  // get params
+  const { params } = context;
+  let movieDetails = {};
+  // url to fetch by id
+  const url = `
+  https://api.themoviedb.org/3/movie/${params.movie}?api_key=86e04c8b56fc4bc4b40c3e45f058a61d&language=en-US`;
+  try {
+    const { data } = await axios.get(url);
+    // set data
+    movieDetails = { ...data };
+  } catch (e) {
+    console.log(e.message, ':::: error message ::::');
+  }
+  if (!movieDetails) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    // pass movieDetails to the page component as props
+    props: { movieDetails },
+  };
+}
 
 export default MovieList;
